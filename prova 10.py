12834e1d713d8992a395c7a7a6a042f5c3f90551
hannaoliveira2007 <hannaboliveiras@gmail.com>
2025-04-14 11:28:07 -0300

    Update prova 10.py

--- prova 10.py
+++ prova 10.py
@@ -1,13 +1,7 @@
-#for x in range(len(palavra) -1, -1, -1):
- #   arvalap += palavra[x]
-
-#print(arvalap)
-
 n = [ 2, 4, 3, 8, 9 ,10, 23, 17, 20, 70, 100, 45, 67, 0, 25, 59]
 n1 = []
 x = 0
 
-for x in range(len(n) -1, -1, -1):
-    n1 += n[x]
-print(n1)
-
+print(n[::-1])
+n.sort()
+print(n)
